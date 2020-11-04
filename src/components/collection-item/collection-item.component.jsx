bb69f510c3fc2ff01b1c46b18281b680bc63f694
Dev <ursprajwal1@gmail.com>
2020-11-04 17:50:29 +0530

    Added styles to Cart and created cart item reducer and adding multiple items to cart

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 
 import './collection-item.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
+const CollectionItem = ({ item, addItem }) => {
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+  const {  name, price, imageUrl } = item
+  return(
   <div className='collection-item'>
     <div
       className='image'
@@ -14,7 +20,13 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className='name'>{name}</span>
       <span className='price'>${price}</span>
     </div>
+    <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
   </div>
-);
+)};
 
-export default CollectionItem;
+const mapDispatchProps = dispatch => ({
+  addItem: item => dispatch(addItem(item))
+})
+export default connect(null, mapDispatchProps)(CollectionItem);
