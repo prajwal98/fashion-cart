39425693eb57fb25e99f2e92c69a543baf8b58f1
Dev <ursprajwal1@gmail.com>
2020-11-05 18:45:10 +0530

    added addItem and removeItem functionality

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -20,8 +20,8 @@ export const selectCartTotal = createSelector(
     [selectCartItems],
     cartItems =>
       cartItems.reduce(
-        (accumalatedQuantity, cartItem) =>
-          accumalatedQuantity + cartItem.quantity * cartItem.price,
+        (accumulatedQuantity, cartItem) =>
+          accumulatedQuantity + cartItem.quantity * cartItem.price,
         0
       )
   );
